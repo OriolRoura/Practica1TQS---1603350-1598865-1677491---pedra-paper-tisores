ea2de074b26c3759cab9f55d05f9b0b88e6dc442
NIU1603350 <73343048+NIU1603350@users.noreply.github.com>
2022-11-11 21:19:29 +0100

    MockValidacio(complet)

--- RockPaperScissorsLizardSpock-master/src/tests/MockValidacion.java
+++ RockPaperScissorsLizardSpock-master/src/tests/MockValidacion.java
@@ -7,113 +7,106 @@ import Modelo.TiposPartida;
 import java.util.Scanner;
 public class MockValidacion implements Validacion{
     private Scanner in = new Scanner(System.in); 
+    int contadorIn=0;
+    int contadorInt = 3;
+    int contadorString =4;
+    int contadorJugada=0;
     public TiposPartida inTipo(){
         TiposPartida tipo=null;                
         int entrada = 0;
         boolean valido = false;
         do {
-            try {
-                in = new Scanner(System.in); 
-                entrada = in.nextInt();
-                valido = true;               
-                if(entrada<5 && entrada>0){                   
-                    switch(entrada){
-                        case 1:
-                            tipo=TiposPartida.JvJ;
-                            break;
-                        case 2:
-                            tipo=TiposPartida.JvB;
-                            break;
-                        case 3:
-                            tipo=TiposPartida.BvB;
-                            break;
-                        case 4:
-                            tipo=TiposPartida.C;
-                            break;
-                        case 5:
-                            System.exit(0);
-                    }                  
-                }
-                else{
-                    System.out.println("Valor de entrada fuera de rango.");
-                    System.out.println("Ingrese uno nuevo:");
-                }
-            }catch (Exception e){
-               this.mostrarError("Entero");                  
+            entrada = contadorIn;
+            valido = true;               
+            if(entrada<5 && entrada>0){                   
+                switch(entrada){
+                    case 1:
+                        tipo=TiposPartida.JvJ;
+                        break;
+                    case 2:
+                        tipo=TiposPartida.JvB;
+                        break;
+                    case 3:
+                        tipo=TiposPartida.BvB;
+                        break;
+                    case 4:
+                        tipo=TiposPartida.C;
+                        break;
+                    case 5:
+                        System.exit(0);
+                } 
+                
             }
+            else{
+                System.out.println("Valor de entrada fuera de rango.");
+                System.out.println("Ingrese uno nuevo:");
+            }
+            contadorIn++;
+            if(contadorIn == 5) {
+            	contadorIn ++;
+            }else if(contadorIn==7) {
+            		contadorIn = 0;
+            }
+                
         }while (!valido);       
         return tipo;
     }
     
     public int inInt(){
-        int entero= 0;
-        boolean valido = false;
-        do {
-            try {
-                in = new Scanner(System.in); 
-                entero = in.nextInt();
-                valido = true;
-            }catch (Exception e){
-               this.mostrarError("Entero mayor o igual a 3.");
-            }
-        }while (!valido);
-        return entero;
+
+        return contadorInt;
     }
+
     
     public String inNombre(){
-        String entrada = "";
-        boolean valido = false;
-        do {
-            in = new Scanner(System.in); 
-            String input = in.next();
-            if (entrada.equals(input)){
-                this.mostrarError("No nulo.");
-            }else{
-                entrada = input;
-                valido = true;
-            }
-        }while (!valido);
-        return entrada;
+    	contadorString++;
+    	if(contadorString == 5) {
+    		contadorString=0;
+    	}
+    	switch(contadorString){
+        case 0:
+        	return (String)"Carmen";
+        case 1:
+        	return (String)"Martina";
+        case 2:
+        	return (String)"Jose";
+        case 3:
+        	return (String)"Pere";
+        default:
+        	return (String)"Montoya";
+        	
+    	}
+    	
     }
     
     public OpcionesJugada inJugada(){
         OpcionesJugada opcion =null;
-        char caracter = ' ';
-        boolean valido = false;
-        do {
-            in = new Scanner(System.in); 
-            String entrada = in.next();
-            if (entrada.length() != 1) {
-                this.mostrarError("caracter");
-            }           
-            else if (entrada.charAt(0) != 'R' && entrada.charAt(0)!= 'P' && entrada.charAt(0) != 'S' && entrada.charAt(0) != 'L' && entrada.charAt(0) != 'V'){
-                this.mostrarError("R o P o S o L o V");
-            }           
-            else {
-                caracter = entrada.charAt(0);
-                valido = true;
-                switch(caracter){
-                    case 'R':
-                        opcion=OpcionesJugada.ROCK;
-                        break;
-                    case 'P':
-                        opcion=OpcionesJugada.PAPER;
-                        break;
-                    case 'S':
-                        opcion=OpcionesJugada.SCISSORS;
-                        break;  
-                    case 'L':
-                        opcion=OpcionesJugada.LIZARD;
-                        break;  
-                    case 'V':
-                        opcion=OpcionesJugada.SPOCK;
-                        break;    
-                }
-            }                      
-        } while (!valido);
+
+		switch(contadorJugada){
+            case 1:
+                opcion=OpcionesJugada.ROCK;
+                break;
+            case 2:
+                opcion=OpcionesJugada.PAPER;
+                break;
+            case 3:
+                opcion=OpcionesJugada.SCISSORS;
+                break;  
+            case 4:
+                opcion=OpcionesJugada.LIZARD;
+                break;  
+            case 5:
+                opcion=OpcionesJugada.SPOCK;
+                break;    
+        }
+		contadorJugada++;
+		if(contadorJugada ==6) {
+			contadorJugada = 0;
+		}
         return opcion;
     }
     
     private void mostrarError(String formato){
         System.out.println("Error de Formato.. "+ "Ingrese un valor con formato: " + formato + "\n" );
     }
+}
